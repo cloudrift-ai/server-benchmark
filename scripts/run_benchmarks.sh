ac596ed9757564cac23783b416a26e4c6eae9d7e
Natalia Trifonova <koshmanova.n@gmail.com>
2025-08-01 21:58:48 -0700

    Fix run_vllm_bechmark script start

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-source ./$SCRIPT_DIR/run_vllm_benchmark.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $SCRIPT_DIR/run_vllm_benchmark.sh
 
 # Run YABS benchmark
 curl -sL https://yabs.sh | bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
 

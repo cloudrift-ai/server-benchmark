84a0b0e0bd9032f260d8782207b63e629b68e698
Natalia Trifonova <koshmanova.n@gmail.com>
2025-08-01 21:44:50 -0700

    Fix comments

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 
 source ./$SCRIPT_DIR/run_vllm_benchmark.sh
 
+# Run YABS benchmark
 curl -sL https://yabs.sh | bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )

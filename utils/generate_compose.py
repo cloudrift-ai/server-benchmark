bce90430b3f08a8ac1dde0a7748f450e0eb469cc
Dmitry Trifonov <slonegg@gmail.com>
2025-10-08 00:01:49 -0700

    fix compose file

--- utils/generate_compose.py
+++ utils/generate_compose.py
@@ -102,7 +102,8 @@ def generate_benchmark_service(hf_directory: str, hf_token: str) -> str:
     environment:
       - HUGGING_FACE_HUB_TOKEN={hf_token}
       - CUDA_VISIBLE_DEVICES=0
-    command: sleep infinity
+    entrypoint: ["/bin/bash", "-c"]
+    command: ["sleep infinity"]
     profiles:
       - tools
 """
